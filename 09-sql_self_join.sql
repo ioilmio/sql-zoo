c4877f619bf1769710baedac256e815aad651563
ioilmio <illuminato.salvatore@gmail.com>
2020-06-13 16:38:04 +0200

    sql self join q#10 on SO

--- 09-sql_self_join.sql
+++ 09-sql_self_join.sql
@@ -93,3 +93,34 @@ Hint
 Self-join twice to find buses that visit Craiglockhart and Lochend, then join those on matching stops.*/    
 
 
+SELECT 
+    firstbus.busnumber AS 'num', 
+    firstbus.company, 
+    secondbus.transfer AS 'name', 
+    secondbus.busnumber AS 'num', 
+    secondbus.company 
+FROM (
+    SELECT r1.num AS 'busnumber', 
+    r1.company AS 'company', 
+    r2.stop AS 'stopp' 
+    FROM route r1 
+    JOIN route r2 ON (r1.num = r2.num AND r1.company = r2.company) 
+    JOIN stops s1 ON s1.id = r1.stop
+    JOIN stops s2 ON s2.id = r2.stop
+    WHERE s1.name = 'Craiglockhart'
+    ) firstbus
+    JOIN
+    (
+    SELECT s1.name AS 'transfer', 
+    r1.num AS 'busnumber', 
+    r1.company AS 'company', 
+    r1.stop AS 'stopp', 
+    r1.pos AS 'pos' 
+    FROM route r1 JOIN route r2 ON (r1.num = r2.num AND r1.company = r2.company) 
+    JOIN stops s1 ON s1.id = r1.stop
+    JOIN stops s2 ON s2.id = r2.stop
+    WHERE s2.name = 'Lochend'
+    ) secondbus
+    ON firstbus.stopp = secondbus.stopp
+ORDER BY firstbus.busnumber, name, 4;
+
